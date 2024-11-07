cf0cb324c1a94c5362493e15d699dab4fbe85ac9
Laurinhaaa <158209993+imlaurinhaaa@users.noreply.github.com>
2024-11-07 16:40:43 -0300

    feat: create new validation post

--- src/routes/estudantes.routes.js
+++ src/routes/estudantes.routes.js
@@ -5,26 +5,51 @@ const studentsRoutes = Router();
 const studentsList = new StudentRepository();
 
 studentsRoutes.get("/", (req, res) => {
-    const students = studentsList.getAllStudents()
-    return res.status(200).json({
-        message:
-        students.length == 0
+  const students = studentsList.getAllStudents();
+  return res.status(200).json({
+    message:
+      students.length == 0
         ? "Nenhum estudante cadastrado"
         : `Total de estudante cadastrados: ${student.length}`,
-        students,
-    });
-    });
+    students,
+  });
+});
 
 studentsRoutes.post("/", (req, res) => {
-  const {materia, progresso, objetivos, dificuldade} = req.body;
+  const { materia, progresso, objetivos, dificuldade } = req.body;
 
-  const students = studentsList.addStudent(materia, progresso, objetivos, dificuldade);
+  const students = studentsList.addStudent(
+    materia,
+    progresso,
+    objetivos,
+    dificuldade
+  );
+
+  if (!materia || !progresso) {
+    return res.status(400).json({
+        message: "Os campos materia e progresso são obrigatórios",
+        students,
+    });
+    }
+
+    if (!objetivos || objetivos.length < 2) {
+        return res.status(400).json({
+            message: "O campo objetivos deve conter no mínimo 2 objetivos",
+            students,
+        });
+    }
+
+    if (dificuldade != "Fácil" && dificuldade != "Médio" && dificuldade != "Difícil") {
+        return res.status(400).json({
+            message: "O campo dificuldade deve ser Fácil, Médio ou Difícil",
+            students,
+        });
+  }
 
   return res.status(201).json({
     message: "Estudante cadastrado com sucesso!",
     students,
   });
 });
-
 
 export default studentsRoutes;
